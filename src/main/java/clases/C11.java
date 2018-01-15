fba3b5581710af6a643427725a82f0f8aae0189d
bo0414 <bo0414@192.168.7.11>
2018-01-15 15:58:38 +0100

    Hotfix 01 error en m1()

--- src/main/java/clases/C11.java
+++ src/main/java/clases/C11.java
@@ -2,8 +2,11 @@ package clases;
 
 public class C11 {
     public static String m1() {
-
-        return "m1";
+        /**
+         * error hotfix 01
+         */
+        String m1 = "m1";
+        return m1;
     }
 
     public static String m2() {
